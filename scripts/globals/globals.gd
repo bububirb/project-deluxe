2082709f34475ccb04f33d88351a6cd3ebc4810f
Kira Kowaltschuk <kirakowaltschuk@proton.me>
2025-04-26 22:23:39 -0400

    added the master script

--- scripts/globals/globals.gd
+++ scripts/globals/globals.gd
@@ -4,5 +4,16 @@ enum ItemMode {ACTIONABLE, USABLE, PASSIVE}
 enum ItemClass {CANNON, MORTAR, SPEED_BOOST}
 
 func projectile_arc(x: float, distance: float, height: float, offset: float):
-	# (x(4hr - 4hx + rv))/r^2
-	return (x * ((4 * height * distance) - (4 * height * x) + (distance * offset))) / pow(distance, 2.0)
+	#hardcoded for now, need to be in definition later
+	var maxrange : float =  45
+	var maxangle : float = 35
+	var minangle : float = -89
+	
+	#keeps maximum range the same with different angles
+	var rangedivisor : float = maxrange / ( tan(maxangle))
+	
+	#OH GLORIOUS Q (slope coefficient)
+	var requiredslope = ((offset) + (pow(distance,2) / rangedivisor)) / distance
+	var q : float = max( min( requiredslope, tan(maxangle)), tan(minangle) )
+	
+	return ((-1 * x * x) / (rangedivisor)) + (q * x)
