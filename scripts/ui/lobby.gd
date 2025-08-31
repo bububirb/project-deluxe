2c3e5c64fe1377fb6dd597ea836be138ca9d1776
Bubu <73459333+bububirb@users.noreply.github.com>
2025-08-31 22:47:22 +0800

    Lobby: Save and Load Configuration
    -  Name, join address and port are saved into the configuration.
    - Simplified lobby UI.

--- scripts/ui/lobby.gd
+++ scripts/ui/lobby.gd
@@ -5,19 +5,19 @@ const LOADING_SCENE = "res://scenes/ui/loading_screen.tscn"
 const PLAYER_SCENE = "res://scenes/drafts/tuggy_player.tscn"
 const GAME_ROOM_SCENE = "res://scenes/ui/game_room.tscn"
 
-@onready var host_button: Button = $CenterContainer/PanelContainer/MarginContainer/LobbyContainer/HBoxContainer/HostButton
-@onready var join_button: Button = $CenterContainer/PanelContainer/MarginContainer/LobbyContainer/HBoxContainer/JoinButton
-@onready var start_button: Button = $CenterContainer/PanelContainer/MarginContainer/HostingContainer/StartButton
-@onready var back_button: Button = $CenterContainer/PanelContainer/MarginContainer/HostingContainer/TitleBar/BackButton
-@onready var exit_button: Button = $CenterContainer/PanelContainer/MarginContainer/JoiningContainer/ExitButton
-@onready var players_container: VBoxContainer = $CenterContainer/PanelContainer/MarginContainer/HostingContainer/PlayersContainer
-@onready var lobby_container: VBoxContainer = $CenterContainer/PanelContainer/MarginContainer/LobbyContainer
-@onready var hosting_container: VBoxContainer = $CenterContainer/PanelContainer/MarginContainer/HostingContainer
-@onready var joining_container: VBoxContainer = $CenterContainer/PanelContainer/MarginContainer/JoiningContainer
-@onready var name_input: LineEdit = $CenterContainer/PanelContainer/MarginContainer/LobbyContainer/HBoxContainer2/NameInput
-@onready var address_input: LineEdit = $CenterContainer/PanelContainer/MarginContainer/LobbyContainer/HBoxContainer3/AddressInput
-@onready var port_input: LineEdit = $CenterContainer/PanelContainer/MarginContainer/LobbyContainer/HBoxContainer4/PortInput
-@onready var ip_label: RichTextLabel = $CenterContainer/PanelContainer/MarginContainer/HostingContainer/IPContainer/IPLabel
+@export var host_button: Button
+@export var join_button: Button
+@export var start_button: Button
+@export var back_button: Button
+@export var exit_button: Button
+@export var settings_button: Button
+@export var settings_container: VBoxContainer
+@export var hosting_container: VBoxContainer
+@export var joining_container: VBoxContainer
+@export var name_input: LineEdit
+@export var address_input: LineEdit
+@export var port_input: LineEdit
+@export var ip_label: RichTextLabel
 
 func _ready() -> void:
 	host_button.pressed.connect(_on_host_button_pressed)
@@ -25,7 +25,10 @@ func _ready() -> void:
 	join_button.pressed.connect(_on_join_button_pressed)
 	back_button.pressed.connect(_on_back_button_pressed)
 	exit_button.pressed.connect(_on_exit_button_pressed)
+	settings_button.pressed.connect(_on_settings_button_pressed)
 	name_input.text_changed.connect(_on_name_input_text_changed)
+	address_input.text_changed.connect(_on_address_input_text_changed)
+	port_input.text_changed.connect(_on_port_input_text_changed)
 	
 	MultiplayerLobby.server_disconnected.connect(_on_multiplayer_lobby_server_disconnected)
 	
@@ -46,9 +49,14 @@ func _ready() -> void:
 			address_input.text = arguments["ip"]
 		join_button.pressed.emit()
 
+	# Load config
+	name_input.text = IO.config.name
+	address_input.text = IO.config.join_address
+	port_input.text = str(IO.config.port)
+
 func _on_host_button_pressed() -> void:
 	multiplayer.multiplayer_peer = null
-	lobby_container.hide()
+	settings_container.hide()
 	_update_ip_label()
 	get_tree().change_scene_to_file(GAME_ROOM_SCENE)
 
@@ -65,20 +73,30 @@ func _on_join_button_pressed() -> void:
 func _on_back_button_pressed() -> void:
 	MultiplayerLobby.remove_multiplayer_peer()
 	hosting_container.hide()
-	lobby_container.show()
+	settings_container.show()
 
 func _on_exit_button_pressed() -> void:
 	MultiplayerLobby.remove_multiplayer_peer()
 	joining_container.hide()
-	lobby_container.show()
+	settings_container.show()
+
+func _on_settings_button_pressed() -> void:
+	settings_container.visible = !settings_container.visible
 
 func _on_start_button_pressed() -> void:
 	MultiplayerLobby.status = MultiplayerLobby.ServerStatus.LOADING
 	MultiplayerLobby.load_game.rpc(LOADING_SCENE)
 
 func _on_name_input_text_changed(new_text: String) -> void:
+	IO.config.name = new_text
 	MultiplayerLobby.player_info.name = new_text
+
+func _on_address_input_text_changed(new_text: String) -> void:
+	IO.config.join_address = new_text
+
+func _on_port_input_text_changed(new_text: String) -> void:
+	IO.config.port = int(new_text)
 
 func _on_multiplayer_lobby_server_disconnected() -> void:
 	joining_container.hide()
-	lobby_container.show()
+	settings_container.show()
