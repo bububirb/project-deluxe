a80591848ba8fcebc0529084eee79ed8f2180f73
Bubu <73459333+bububirb@users.noreply.github.com>
2025-09-14 03:38:37 +0800

    Fix: Rewrite Mortar Arc

--- scripts/projectiles/projectile_arc/mortar_arc.gd
+++ scripts/projectiles/projectile_arc/mortar_arc.gd
@@ -1,34 +1,18 @@
 class_name MortarArc extends ProjectileArc
 
 # Arc parameters
-var arc_angle: float
-var arc_range: float
-var arc_slope: float
+var a: float
+var b: float
+var c: float
 
 func _init(projectile_stats: ProjectileStats) -> void:
 	stats = projectile_stats
 	_calculate_arc()
 
-func clamp_slope() -> MortarArc:
-	arc_slope = max(min(arc_slope, tan(stats.ballistics.max_angle)), tan(stats.ballistics.min_angle))
-	return self
-
 func arc_height(x: float):
-	return (-pow(x, 2.0) / arc_range) + (arc_slope * x)
-
+	return (a * pow(x, 2.0) + b * x + c)
 
 func _calculate_arc() -> void:
-	arc_angle = _calculate_arc_angle()
-	arc_range = _calculate_arc_range()
-	arc_slope = _calculate_arc_slope()
-
-func _calculate_arc_angle() -> float:
-	var angle = (asin(-stats.distance / stats.ballistics.max_range) + PI) / 2.0
-	angle = max(min(angle, stats.ballistics.max_angle), stats.ballistics.min_angle)
-	return angle
-
-func _calculate_arc_range() -> float:
-	return (2.0 * stats.ballistics.max_range * pow(cos(arc_angle), 2.0))
-
-func _calculate_arc_slope() -> float:
-	return tan(arc_angle)
+	a = -4 * stats.ballistics.height / pow(stats.distance, 2.0)
+	b = 4 * stats.ballistics.height / stats.distance
+	c = stats.offset / stats.distance
