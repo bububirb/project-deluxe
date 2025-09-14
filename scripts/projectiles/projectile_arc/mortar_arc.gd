1823101b76cd4ea7e252b8fcbe63b59348da5068
Bubu <73459333+bububirb@users.noreply.github.com>
2025-09-14 14:18:53 +0800

    Items: Implement New Mortar Arc (#29)
    - Mortars have the same travel time regardless of distance.
    - New Mortar Ballistics use minimum and maximum range instead of angles.

--- scripts/projectiles/projectile_arc/mortar_arc.gd
+++ scripts/projectiles/projectile_arc/mortar_arc.gd
@@ -1,34 +1,20 @@
 class_name MortarArc extends ProjectileArc
 
 # Arc parameters
-var arc_angle: float
-var arc_range: float
-var arc_slope: float
+var a: float
+var b: float
+var c: float
+var distance: float
 
 func _init(projectile_stats: ProjectileStats) -> void:
 	stats = projectile_stats
 	_calculate_arc()
 
-func clamp_slope() -> MortarArc:
-	arc_slope = max(min(arc_slope, tan(stats.ballistics.max_angle)), tan(stats.ballistics.min_angle))
-	return self
-
 func arc_height(x: float):
-	return (-pow(x, 2.0) / arc_range) + (arc_slope * x)
-
+	return (a * pow(x, 2.0) + b * x + c)
 
 func _calculate_arc() -> void:
-	arc_angle = _calculate_arc_angle()
-	arc_range = _calculate_arc_range()
-	arc_slope = _calculate_arc_slope()
-
-func _calculate_arc_angle() -> float:
-	var angle = (asin(-stats.distance / stats.ballistics.max_range) + PI) / 2.0
-	angle = max(min(angle, stats.ballistics.max_angle), stats.ballistics.min_angle)
-	return angle
-
-func _calculate_arc_range() -> float:
-	return (2.0 * stats.ballistics.max_range * pow(cos(arc_angle), 2.0))
-
-func _calculate_arc_slope() -> float:
-	return tan(arc_angle)
+	distance = clampf(stats.distance, stats.ballistics.min_range, stats.ballistics.max_range)
+	a = -4 * stats.ballistics.height / pow(distance, 2.0)
+	b = 4 * stats.ballistics.height / distance
+	c = stats.offset / distance
--- scripts/resources/ballistics/mortar_ballistics.gd
+++ scripts/resources/ballistics/mortar_ballistics.gd
@@ -1,6 +1,6 @@
 class_name MortarBallistics extends Ballistics
 
-@export var projectile_speed: float = 30.0
+@export var travel_time: float = 3.0
+@export var min_range: float = 5.0
 @export var max_range: float = 45.0
-@export_range(-90, 90, 0.1, "radians_as_degrees") var min_angle: float
-@export_range(-90, 90, 0.1, "radians_as_degrees") var max_angle: float
+@export var height: float = 30.0
