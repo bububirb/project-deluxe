8da8a2003a731f50bbc26288435d6f4c9f3375b3
Bubu <73459333+bububirb@users.noreply.github.com>
2025-10-25 01:16:11 +0800

    Fix: Ship Sprites Not Loading on Export

--- scripts/ui/ship_button.gd
+++ scripts/ui/ship_button.gd
@@ -8,11 +8,9 @@ const SHIP_SPRITES_PATH: String = "res://assets/sprites/ships/"
 		ship_name = new_ship_name
 		for child in get_children():
 			child.queue_free()
-		var dir = DirAccess.open(SHIP_SPRITES_PATH)
-		if dir.file_exists(ship_name + ".png"):
-			var sprite: Texture2D = load(SHIP_SPRITES_PATH + ship_name + ".png")
-			if sprite:
-				icon = sprite
+		var sprite: Texture2D = load(SHIP_SPRITES_PATH + ship_name + ".png")
+		if sprite:
+			icon = sprite
 
 func _ready() -> void:
 	pressed.connect(MultiplayerLobby._on_player_ship_set.bind(ship_name))
--- scripts/ui/ship_sprite.gd
+++ scripts/ui/ship_sprite.gd
@@ -8,13 +8,11 @@ const SHIP_SPRITES_PATH: String = "res://assets/sprites/ships/"
 		ship_name = new_ship_name
 		for child in get_children():
 			child.queue_free()
-		var dir = DirAccess.open(SHIP_SPRITES_PATH)
-		if dir.file_exists(ship_name + ".png"):
-			var sprite: Texture2D = load(SHIP_SPRITES_PATH + ship_name + ".png")
-			if sprite:
-				var texture: TextureRect = TextureRect.new()
-				texture.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-				texture.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
-				texture.texture = sprite
-				texture.mouse_filter = Control.MOUSE_FILTER_IGNORE
-				add_child(texture)
+		var sprite: Texture2D = load(SHIP_SPRITES_PATH + ship_name + ".png")
+		if sprite:
+			var texture: TextureRect = TextureRect.new()
+			texture.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+			texture.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+			texture.texture = sprite
+			texture.mouse_filter = Control.MOUSE_FILTER_IGNORE
+			add_child(texture)
