c6982761239adbe2ea5fc5adea6cc8e2ee0147c9
Bubu <73459333+bububirb@users.noreply.github.com>
2025-10-24 14:47:41 +0800

    Fix: Load Workshop Deck from Player Info

--- scripts/ui/inventory.gd
+++ scripts/ui/inventory.gd
@@ -1,10 +1,19 @@
-extends HFlowContainer
+class_name Inventory extends HFlowContainer
 
 @export var slot_scene: PackedScene
 @export var item_container_scene: PackedScene
-@export var items: Array[Item]
+@export var items: Array[Item]:
+	set(new_items):
+		items = new_items
+		update_item_slots()
 
-func _ready() -> void:
+func remove_item(item: Item) -> void:
+	items.erase(item)
+	update_item_slots()
+
+func update_item_slots() -> void:
+	for child: WorkshopSlot in get_children():
+		child.queue_free()
 	for item: Item in items:
 		var slot: WorkshopSlot = slot_scene.instantiate()
 		slot.item = item
--- scripts/ui/workshop.gd
+++ scripts/ui/workshop.gd
@@ -1,8 +1,14 @@
 extends PanelContainer
 
 @export var deck: Control
+@export var inventory: Control
 
 func _ready() -> void:
+	if MultiplayerLobby.players:
+		var player_deck: Array[String]
+		player_deck.assign(MultiplayerLobby.players[multiplayer.get_unique_id()].deck.values())
+		for i in player_deck.size():
+			set_item_slot(i, player_deck[i])
 	for workshop_slot: WorkshopSlot in deck.get_children():
 		workshop_slot.item_set.connect(_on_workshop_slot_item_set)
 
@@ -11,3 +17,8 @@ func _on_workshop_slot_item_set(index: int, item: Item) -> void:
 	if item:
 		item_name = item.item_name
 	MultiplayerLobby.set_player_deck_item.rpc(index, item_name)
+
+func set_item_slot(index: int, item_name: String) -> void:
+	var item: Item = load("res://resources/items/" + item_name + ".tres")
+	deck.get_child(index).item = item
+	inventory.remove_item(item)
