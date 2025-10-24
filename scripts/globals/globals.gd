cd8552b49331d6e9a5895771d38e984b5d66e9db
Bubu <73459333+bububirb@users.noreply.github.com>
2025-10-24 16:56:01 +0800

    Core: Add Support for Display Scaling

--- scripts/globals/globals.gd
+++ scripts/globals/globals.gd
@@ -6,6 +6,9 @@ enum ItemMode {ACTIONABLE, USABLE, PASSIVE}
 enum ItemClass {CANNON, MORTAR, BEAM, TORPEDO, SPEED_BOOST}
 enum ModifierType {BUFF, DEBUFF}
 
+func _init() -> void:
+	ProjectSettings.set_setting("display/window/stretch/scale", DisplayServer.screen_get_scale())
+
 func projectile_arc(x: float, distance: float, offset: float, max_range: float, min_angle: float, max_angle: float):
 	# Keep maximum range the same with different angles
 	var arc_range: float = max_range / tan(max_angle)
