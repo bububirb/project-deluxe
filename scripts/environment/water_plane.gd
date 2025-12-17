b6bae3e3ea4741b032da3360c67f5dd505a6f698
Bubu <73459333+bububirb@users.noreply.github.com>
2025-12-18 03:00:33 +0800

    Refactor: Prefix Unused Delta in water_plane.gd

--- scripts/environment/water_plane.gd
+++ scripts/environment/water_plane.gd
@@ -1,6 +1,6 @@
 extends MeshInstance3D
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	global_position.x = BuoyancySolver.player_position.x
 	global_position.z = BuoyancySolver.player_position.z
 	RenderingServer.global_shader_parameter_set("ocean_pos", global_position)
