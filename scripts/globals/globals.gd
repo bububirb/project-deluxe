aa183559483a5552282000297e2767ab38bdd216
Bubu <73459333+bububirb@users.noreply.github.com>
2026-01-15 07:23:25 +0800

    Refactor: Remove Global Projectile Arc Function

--- scripts/globals/globals.gd
+++ scripts/globals/globals.gd
@@ -9,12 +9,3 @@ enum ModifierType {BUFF, DEBUFF}
 func _init() -> void:
 	ProjectSettings.set_setting("display/window/stretch/scale", DisplayServer.screen_get_scale())
 
-func projectile_arc(x: float, distance: float, offset: float, max_range: float, min_angle: float, max_angle: float):
-	# Keep maximum range the same with different angles
-	var arc_range: float = max_range / tan(max_angle)
-	
-	# OH GLORIOUS Q - slope coefficient
-	var slope = (offset + (pow(distance, 2.0) / arc_range)) / distance
-	var q: float = max(min(slope, tan(max_angle)), tan(min_angle))
-	
-	return (-pow(x, 2.0) / arc_range) + (q * x)
