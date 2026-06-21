c2ad98f75c09a979af9e6edde57ed4b1334cf461
Bubu <73459333+bububirb@users.noreply.github.com>
2026-06-21 17:13:44 +0800

    Cleanup: Remove Deprecated Weapon Stats

--- scripts/resources/stats/weapon_stats.gd
+++ scripts/resources/stats/weapon_stats.gd
@@ -10,5 +10,3 @@ class_name WeaponStats extends Stats
 @export var modifiers_on_use: Array[Modifier] # List of modifiers to apply on self when used
 @export var modifiers_on_hit: Array[Modifier] # List of modifiers to apply on target when hit
 
-@export_group("Deprecated")
-@export var height: float
